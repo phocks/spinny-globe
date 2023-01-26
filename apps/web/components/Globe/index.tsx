b4d797999cfa44cf8a8ea3f053f6d53d133fa319
Joshua Byrd <phocks@gmail.com>
2023-01-26 14:46:08 +1000

    Seems to work

--- apps/web/components/Globe/index.tsx
+++ apps/web/components/Globe/index.tsx
@@ -1,12 +1,17 @@
 import { useEffect, useRef } from "react";
-import { mountGlobe } from "globe-vis";
+import { mountGlobe, unmountGlobe } from "globe-vis";
 
 export default function Globe() {
   const inputEl = useRef(null);
 
   useEffect(() => {
-    console.log("ok");
-    mountGlobe({ mountTo: inputEl.current });
+    const container: any = inputEl.current;
+    mountGlobe({ mountTo: container });
+
+    // Clean up on unmount
+    return () => {
+      if (container) container.innerHTML = "";
+    };
   }, []);
 
   return <div ref={inputEl} className="spinny-globe" />;
